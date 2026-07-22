c8082d8f59e1ddd925d77a2c657aef8b7748cfff
Neerajpatel07 <neerajpatel72725@gmail.com>
2026-07-22 12:19:57 +0530

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>🚀 ToolMint AI</h1>
+      <p>Welcome to the Future of AI Tools</p>
+
+      <button>Explore Tools</button>
+    </div>
+  );
+}
+
+export default App;
