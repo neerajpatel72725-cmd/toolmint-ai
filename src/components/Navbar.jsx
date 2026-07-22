b2d7efe7927d87665d53c3cafbe799d022d9b9b7
Neerajpatel07 <neerajpatel72725@gmail.com>
2026-07-23 00:52:52 +0530

    Create Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>🚀 ToolMint AI</h2>
+
+      <div className="menu">
+        <a href="#">Home</a>
+        <a href="#">Trending</a>
+        <a href="#">Categories</a>
+        <a href="#">About</a>
+      </div>
+    </nav>
+  );
+}
